c5d15c5b7caecfd913e2ef395b09429ffe367bb6
Zaratee <39096560+Zaratee@users.noreply.github.com>
2024-09-20 22:56:13 -0600

    fix img not showing in production vercel

--- src/data/userProfiles.tsx
+++ src/data/userProfiles.tsx
@@ -1,51 +1,44 @@
-import user1Img from 'src/assets/img/user1.png'
-import user2Img from 'src/assets/img/user2.png'
-import user3Img from 'src/assets/img/user3.png'
-import user4Img from 'src/assets/img/user4.png'
-import user5Img from 'src/assets/img/user5.png'
-import user6Img from 'src/assets/img/user6.png'
-import user7Img from 'src/assets/img/user7.png'
-import user8Img from 'src/assets/img/user8.png'
+
 
 export const allAvatars = [
     {
         id: '1',
         name: 'Profile 1',
-        img: user1Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912591829930120/user1.png?ex=66efa23f&is=66ee50bf&hm=43982cef9a01bf49dfefb6c50b522b86290321e6f9f075f0be751b429c1f7387&=&format=webp&quality=lossless&width=1008&height=1008'
     },
     {
         id: '2',
         name: 'Profile 2',
-        img: user2Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912592199290951/user2.png?ex=66efa23f&is=66ee50bf&hm=69c9f504934b4af697d474c96ec4edc7e758e4109d48ecdd677fb31abfd45811&=&format=webp&quality=lossless&width=1008&height=1008'
     },
     {
         id: '3',
         name: 'Profile 3',
-        img: user3Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912592727769108/user3.png?ex=66efa23f&is=66ee50bf&hm=b889241a4ee7fc2a47c54897fa06253ce6460a2ad2ab5bb30f1ea5f4b34beab0&=&format=webp&quality=lossless&width=1008&height=1008'
     },
     {
         id: '4',
         name: 'Profile 4',
-        img: user4Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912592991752293/user4.png?ex=66efa23f&is=66ee50bf&hm=de786f129fc8f610d527deb0ce35c5762747945cafcaf523de7315caadd74241&=&format=webp&quality=lossless&width=1008&height=1008'
     },
     {
         id: '5',
         name: 'Profile 5',
-        img: user5Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912593264508979/user5.png?ex=66efa23f&is=66ee50bf&hm=3560118aebc93c77e7803fb96fbf1bb90928fe2c839a8d5f50c14f50afff9f37&=&format=webp&quality=lossless&width=1008&height=1008'
     },
     {
         id: '6',
         name: 'Profile 6',
-        img: user6Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912593520365578/user6.png?ex=66efa23f&is=66ee50bf&hm=28b75000ada11d1f6132b860faab347a58e083e8399da7ef1b72176f4440cd1d&=&format=webp&quality=lossless&width=1008&height=1008'
     },
     {
         id: '7',
         name: 'Profile 7',
-        img: user7Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912593897848852/user7.png?ex=66efa240&is=66ee50c0&hm=8bb20248ad85a6838a1b270112c6540ec4784e326b468f05734a6722923dddb7&=&format=webp&quality=lossless&width=1008&height=1008'
     },
     {
         id: '8',
         name: 'Profile 8',
-        img: user8Img
+        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912594191319123/user8.png?ex=66efa240&is=66ee50c0&hm=dbaad4bba7aadf3e9f148ecdf92057e6a6b49121881f889cb32c78c856bb02db&=&format=webp&quality=lossless&width=1008&height=1008'
     },
 ]
