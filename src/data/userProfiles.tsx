f241650f333fc1b76fb7efcd4884a40d3b282a34
Zaratee <39096560+Zaratee@users.noreply.github.com>
2024-10-08 13:23:03 -0600

    added image to public folder

--- src/data/userProfiles.tsx
+++ src/data/userProfiles.tsx
@@ -4,41 +4,41 @@ export const allAvatars = [
     {
         id: '1',
         name: 'Profile 1',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912591829930120/user1.png?ex=66fb7fbf&is=66fa2e3f&hm=b2e831a805e7fbae28acb3c8d93d2594ead38397973efc4b3280b833d68d4502&=&format=webp&quality=lossless&width=936&height=936'
+        img: '/user1.png'
     },
     {
         id: '2',
         name: 'Profile 2',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912592199290951/user2.png?ex=66fb7fbf&is=66fa2e3f&hm=8ec85968621e1ee1f0fc3021e9a0dfa5c16e23199a7350871fb41059fcddc89f&=&format=webp&quality=lossless&width=936&height=936'
+        img: '/user2.png'
     },
     {
         id: '3',
         name: 'Profile 3',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912592727769108/user3.png?ex=66efa23f&is=66ee50bf&hm=b889241a4ee7fc2a47c54897fa06253ce6460a2ad2ab5bb30f1ea5f4b34beab0&=&format=webp&quality=lossless&width=1008&height=1008'
+        img: '/user3.png'
     },
     {
         id: '4',
         name: 'Profile 4',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912592991752293/user4.png?ex=66efa23f&is=66ee50bf&hm=de786f129fc8f610d527deb0ce35c5762747945cafcaf523de7315caadd74241&=&format=webp&quality=lossless&width=1008&height=1008'
+        img: '/user4.png'
     },
     {
         id: '5',
         name: 'Profile 5',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912593264508979/user5.png?ex=66efa23f&is=66ee50bf&hm=3560118aebc93c77e7803fb96fbf1bb90928fe2c839a8d5f50c14f50afff9f37&=&format=webp&quality=lossless&width=1008&height=1008'
+        img: '/user5.png'
     },
     {
         id: '6',
         name: 'Profile 6',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912593520365578/user6.png?ex=66efa23f&is=66ee50bf&hm=28b75000ada11d1f6132b860faab347a58e083e8399da7ef1b72176f4440cd1d&=&format=webp&quality=lossless&width=1008&height=1008'
+        img: '/user6.png'
     },
     {
         id: '7',
         name: 'Profile 7',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912593897848852/user7.png?ex=66efa240&is=66ee50c0&hm=8bb20248ad85a6838a1b270112c6540ec4784e326b468f05734a6722923dddb7&=&format=webp&quality=lossless&width=1008&height=1008'
+        img: '/user7.png'
     },
     {
         id: '8',
         name: 'Profile 8',
-        img: 'https://media.discordapp.net/attachments/852334128635969596/1286912594191319123/user8.png?ex=66efa240&is=66ee50c0&hm=dbaad4bba7aadf3e9f148ecdf92057e6a6b49121881f889cb32c78c856bb02db&=&format=webp&quality=lossless&width=1008&height=1008'
+        img: '/user8.png'
     },
 ]
--- src/page/userDetails/components/weatherDetails/weatherDetails.tsx
+++ src/page/userDetails/components/weatherDetails/weatherDetails.tsx
@@ -20,7 +20,7 @@ export const WeatherDetails = (props: IProps) => {
   const {humidity, wind_kph, wind_degree, wind_dir, uv,  vis_km, name, date, temp_c, condition} = props.locationWeather
   return (
     <div className="weather-details">
-      <img src="https://media.discordapp.net/attachments/852334128635969596/1287285524494815262/bgWeatherDetails.png?ex=66fb8991&is=66fa3811&hm=3d41ffe22c4f7ef9a206151ad6c5a0ef85d3458c569077b45dcafcac497577de&=&format=webp&quality=lossless&width=478&height=934"/>
+      <img src="/bgWeatherDetails.png"/>
       <div className="weather-details-body">
         <div className="date">{date}</div>
         <div className="headline-desktop1">{temp_c} °C</div>
